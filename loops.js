7cc708dd35edf88561a4faa5ecffb1dccd60b701
Arlene George <aemg@live.com>
2019-07-16 01:23:23 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,4 +1,6 @@
 function forLoop(array){
-  for (var = 0; i < 25);
-  if (i===)
+  for (var i = 0; i < 25; i++);
+  if (i===1)
+  return "I am 1 strange loop."
+  else return(`I am ${i} strange loop.`)
 }
